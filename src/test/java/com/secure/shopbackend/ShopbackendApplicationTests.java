bdc24d5738987a6f7d3fa8cf1b934039656b5126
ch9729 <karlll@naver.com>
2025-02-05 14:29:11 +0900

    레퍼지토리 생성

---
+++ src/test/java/com/secure/shopbackend/ShopbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.shopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

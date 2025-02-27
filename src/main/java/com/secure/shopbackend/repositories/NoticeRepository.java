648f5274229b6f1160d6f96c031475082f445eb8
ch9729 <karlll@naver.com>
2025-02-27 11:37:01 +0900

    회원수정 패스워드 암호화

--- src/main/java/com/secure/shopbackend/repositories/NoticeRepository.java
+++ src/main/java/com/secure/shopbackend/repositories/NoticeRepository.java
@@ -9,11 +9,15 @@ import java.util.Optional;
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     Notice save(Notice notice);
 
+    // id값으로 비교
     Optional<Notice> findById(Long id);
 
+    // id가 참인지 거짓인지 비교
     boolean existsById(Long id);
 
+    // 삭제
     void deleteById(Long id);
 
+    // 전체 출력
     List<Notice> findAll();
 }

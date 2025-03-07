19a15e801276fbe4ec3bbb468b1dd415e58c1987
Starling <jimmy000501@naver.com>
2025-03-07 12:28:56 +0900

    구매확정 추가

---
+++ src/main/java/com/secure/shopbackend/repositories/PurchaseRepository.java
@@ -0,0 +1,14 @@
+package com.secure.shopbackend.repositories;
+
+import com.secure.shopbackend.dtos.Purchase;
+import com.secure.shopbackend.dtos.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
+    // 특정 유저의 구매 내역 조회
+    List<Purchase> findAllByUser(User user);
+
+    // 특정 유저와 상품으로 구매 목록 찾기
+    Purchase findByUserAndProduct_ProductId(User user, int productId);
+}
